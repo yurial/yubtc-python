40951a4929c8d1d13df31d9f503af28fb4f9efb9
Yuri Dyachenko <pub.yurial@gmail.com>
2026-08-04 11:03:32 +0300

    fixup tests

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import pytest
 @pytest.mark.parametrize(
         'compressed, seed, privhex, privwif, address',
         [
-            (False, 'qwe', '1814825e69d2e72eabfbec9c0168f5689dcc26509aa2a8590d859a90402f0495', b'5Hztg9Lf6fPida3GtdxhzmC6gTh98oQ6dGPotiFWMBSauUioQxj', b'1HXSiiE8wqH7rtqXW3duSWLBjm4v8XZNX8'),
-            (True, 'qwe', '1814825e69d2e72eabfbec9c0168f5689dcc26509aa2a8590d859a90402f0495', b'Kx2X5mom9zTGkQq38v8swx3z5ApAuRnwq4wfyF52Y55veC7Ce5dz', b'1A3XHZzcxp3bC62T21DQhXZuA6GdzVYxP9'),
+            (False, 'qwe', '1814825e69d2e72eabfbec9c0168f5689dcc26509aa2a8590d859a90402f0455', b'5Hztg9Lf6fPida3GtdxhzmC6gTh98oQ6dGPotiFWMBSanCVcqBb', b'16toxZ1pUrKbw7Ripem1X4aGxmY6b5qSCz'),
+            (True, 'qwe', '1814825e69d2e72eabfbec9c0168f5689dcc26509aa2a8590d859a90402f0455', b'Kx2X5mom9zTGkQq38v8swx3z5ApAuRnwq4wfyF52Y55v6Ke5dRq5', b'1NHD3xcMHK7QW1bPQq1J5SCb6cpbMsCX7k'),
             (False, '12345', '28820488de48082a13c570e68e1295e0207c6ef826a685c220d10fd6d8b95d49', b'5J88JQkRPEffAwVL73kwtDzGFqtBFiCsFXajzb9ytmCZbs4VSUY', b'1MN1fFX2xmKS1qZXyhw5EUpS9Laa2HaeYX'),
             (True, '12345', '28820488de48082a13c570e68e1295e0207c6ef826a685c220d10fd6d8b95d49', b'KxaTDqped9KdUsW3KhAyF6KkLWktFvsNo7yvmBke7U62tWmMs8dk', b'1sW6JDNWppzUjQr8jjQ9KJmVx92ooKEd6'),
         ]
