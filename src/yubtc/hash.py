5da5729e926b70772f028fd1b22f103e9a892273
Yuri Dyachenko <pub.yurial@gmail.com>
2026-08-04 21:07:57 +0300

    Use pycryptodome instead of safe-pysha3

--- src/yubtc/hash.py
+++ src/yubtc/hash.py
@@ -1,22 +1,26 @@
 
 def keccak256(data: bytes) -> bytes:
-    from sha3 import keccak_256
-    return keccak_256(data).digest()
+    from Crypto.Hash import keccak
+    h = keccak.new(digest_bits=256)
+    h.update(data)
+    return h.digest()
 
 
 def sha256(data: bytes) -> bytes:
-    from hashlib import sha256
-    return sha256(data).digest()
+    from Crypto.Hash import SHA256
+    return SHA256.new(data).digest()
 
 
 def ripemd160(data: bytes) -> bytes:
-    import hashlib
-    return hashlib.new('ripemd160', data).digest()
+    from Crypto.Hash import RIPEMD160
+    return RIPEMD160.new(data).digest()
 
 
 def blake2b256(data: bytes) -> bytes:
-    from hashlib import blake2b
-    return blake2b(data, digest_size=32).digest()
+    from Crypto.Hash import BLAKE2b
+    h = BLAKE2b.new(digest_bytes=32)
+    h.update(data)
+    return h.digest()
 
 
 def hash160(data: bytes) -> bytes:
